e5807e86c6d65a316575f853e83206496cb3a981
Lyte <toolxander@gmail.com>
2026-05-18 21:42:14 -0400

    Update countryball.py

--- ballsdex/packages/countryballs/countryball.py
+++ ballsdex/packages/countryballs/countryball.py
@@ -116,7 +116,7 @@ class BallSpawnView(View):
     """
 
     def __init__(self, bot: "BallsDexBot", model: Ball):
-        super().__init__()
+        super().__init__(timeout=360)
         self.bot = bot
         self.model = model
         self.algo: str | None = None
